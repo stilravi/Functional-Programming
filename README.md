ef36e5a75e5152e7dde2c80f1c655cadd7fd985d
Ravi Stil <ravi.raaf@gmail.com>
2021-10-31 17:09:42 +0100

    It's working again 🎉

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Functional-Programming
 Repository for Functional Programming
+
+Tijdelijk even hier gezet:
+
+```
+fetch("./dataset/dataset.json")
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(object => {
+        const result = removeQuestionMarks(object['Kaas is ook een zoogdier?']);
+        console.log(result);
+    })
+  });
+
+function removeQuestionMarks(data) {
+    const stringify = String(data);
+    return stringify.replaceAll('.', '').replaceAll('?', '').toLowerCase();;
+}
+
+// function testFunction(data) {
+//     // const text = document.getElementById('demo');
+//     console.log()
+//     // text.innerHTML = data[0];
+// }
+```
--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,99 @@
-
 fetch("./dataset/dataset.json")
-  .then(response => response.json())
-  .then(data => {
-    data.forEach(object => {
-        const result = removeQuestionMarks(object['Kaas is ook een zoogdier?']);
-        console.log(result);
-    })
+.then(response => response.json())
+.then(data => {
+  data.forEach(object => {
+      let result = changeToString(object['Kaas is ook een zoogdier?']);
+      result = removeQuestionMarks(result)
+      result = removeDots(result)
+      result = removeCapitals(result)
+      console.log(result);
+      printResults(result);
   });
+  
+});
 
-function removeQuestionMarks(data) {
-    const stringify = String(data);
-    return stringify.replaceAll('.', '').replaceAll('?', '').toLowerCase();;
+function changeToString(data) {
+  let stringify = String(data);
+  return stringify;
 }
+
+function removeQuestionMarks(str) {
+  return str.replaceAll('?', '');
+}
+
+function removeDots(str) {
+  return str.replace(/\.+$/, "");
+  // https://stackoverflow.com/questions/20925157/remove-dot-sign-from-the-end-of-the-string
+}
+
+function removeCapitals(str) {
+  return str.toLowerCase();
+}
+
+function printResults(str) {
+  const list = document.getElementById("list");
+  list.innerHTML += `<li>${str}</li>`;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// fetch("./dataset/dataset.json")
+//   .then(response => response.json())
+//   .then(data => {
+//     data.forEach(object => {
+//         let result = removeQuestionMarks(object['Kaas is ook een zoogdier?']);
+//         console.log(result);
+//     })
+//   });
+
+// function removeQuestionMarks(data) {
+//   let stringify = String(data);
+//   return stringify.replaceAll('.', '').replaceAll('?', '').toLowerCase();;
+// }
 
 // function testFunction(data) {
 //     // const text = document.getElementById('demo');
 //     console.log()
 //     // text.innerHTML = data[0];
+// }
+
+
+
+// fetch("./dataset/dataset.json")
+//   .then(response => response.json())
+//   .then(data => {
+//     data.forEach(object => {
+//         let result1 = removeQuestionMarks(object['Kaas is ook een zoogdier?']);
+//         let result2 = removeDots(object['Kaas is ook een zoogdier?']);
+//         console.log(result1, result2);
+//     })
+//   });
+
+// function removeQuestionMarks(string) {
+//   return string.replaceAll('?', '')
+// }
+
+// function removeDots(string) {
+//   return string.replaceAll('.', '')
+// }
+
+
+
+// function removeCapitals(data) {
+
+// }
+
+// function removeQuestionMarks(data) {
+
 // }
 
