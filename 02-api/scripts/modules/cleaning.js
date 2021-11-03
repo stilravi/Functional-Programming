5a777ab7a1eb30ba09c73e220dea7ca18dadeaba
Ravi Stil <ravi.raaf@gmail.com>
2021-11-03 15:19:45 +0100

    Added more comments

--- 02-api/scripts/modules/cleaning.js
+++ 02-api/scripts/modules/cleaning.js
@@ -1,5 +1,5 @@
 function removeDots(str) {
-    return str.replaceAll('.', '')
+    return str.replaceAll('.', '') // Vervang alle punten voor niks in de meegegeven parameter (beers.tagline) en stuur deze terug
 }
 
-export default removeDots
+export default removeDots // Exporteer deze functie zodat deze gebruikt kan worden in render.js
--- 02-api/scripts/modules/render.js
+++ 02-api/scripts/modules/render.js
@@ -1,17 +1,19 @@
-import removeDots from './cleaning.js'
+import removeDots from './cleaning.js' // Importeer removeDots functie van cleaning.js
 
 function showResults(data) {
-    const html = data.map(beers => {
-        let beerTagline = removeDots(beers.tagline)
+    const html = data.map(beers => { // 
+        let beerTagline = removeDots(beers.tagline) // Maakt een let aan voor de beers.tagline. Voer vervolgens de functie removeDots uit.
+        // Return een HTML <div> met daarin een img van het biertje, de naam van het biertje en de tagline (zonder punten).
         return `
-        <div>
+        <li>
             <img src="${beers.image_url}" alt="${beers.name}">
             <h3>${beers.name}</h3>
             <p>${beerTagline}</p>
-        </div>
+        </li>
         `
-    }).join('')
+    }).join('') // Maakt er een string van
+    // Pak het HTML element met de id #app en inset de "const html" direct na het begin van het element
     document.querySelector('#app').insertAdjacentHTML('afterbegin', html)
 }
 
-export default showResults
+export default showResults // Exporteer de functie showResults
